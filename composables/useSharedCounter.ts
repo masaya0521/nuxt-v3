3e90fff74a7c3b72ea7d910d789ba7066217b130
masaya0521 <kaji0521km@gmail.com>
2022-06-26 14:22:06 +0900

    add cmment

--- composables/useSharedCounter.ts
+++ composables/useSharedCounter.ts
@@ -3,7 +3,8 @@ export const inc = (counter: Ref<number>) => () => counter.value++;
 export const dec = (counter: Ref<number>) => () => counter.value--;
 
 export const useSharedCounter = () => {
-  const counter = useState("counter", () => 0);
+  // ここのuseState宣言した変数がグローバルな値
+  const counter = useState<number>("counter", () => 0);
   return {
     counter: readonly(counter),
     dec: dec(counter),
